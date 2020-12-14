98569bce41bacb411ac68270cc34461914d24a14
Jovan Zoric <jzjovanzoric@gmail.com>
2020-12-14 18:41:48 +0100

    Bootstrap + create post

---
+++ resources/views/posts/create.blade.php
@@ -0,0 +1,7 @@
+@extends('layout.app')
+
+@section('title', 'Create post')
+
+@section('content')
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -5,5 +5,5 @@ use App\Models\Post;
 use App\Http\Controllers\PostsController;
 
 Route::get('/', [PostsController::class, 'index']);
-
+Route::get('/posts/create', [PostsController::class, 'create']);
 Route::get('/posts/{id}', [PostsController::class, 'show'])->name('posts.single');
